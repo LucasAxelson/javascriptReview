04b13b4a6bc6959b9e02b7e93009af7a386abc81
Lucas <Lucasaxelson2016@gmail.com`>
2023-04-24 20:01:43 +0100

    Pig Game finalized

--- PigGame/script.js
+++ PigGame/script.js
@@ -1,35 +1,41 @@
 'use strict';
-const newGameBtn = document.querySelector(`.btn--new`);
-const rollDiceBtn = document.querySelector(`.btn--roll`);
-const holdDiceBtn = document.querySelector(`.btn--hold`);
-const dice = document.querySelector(`.dice`);
 
-let rolledScoreOne = document.getElementById(`score--0`);
-let rolledScoreTwo = document.getElementById(`score--1`);
-
-let currentScoreOne = document.getElementById(`current--0`);
-let currentScoreTwo = document.getElementById(`current--1`);
-
-function resetScore() {
-  rolledScoreOne.textContent = 0;
-  rolledScoreTwo.textContent = 0;
-
-  currentScoreTwo.textContent = 0;
-  currentScoreTwo.textContent = 0;
-}
-
-function diceRoll() {
-  let dice = Math.trunc(Math.random() * 6 + 1);
-  return dice;
-}
-
-resetScore();
+resetGame();
 
 document.querySelector(`.btn--roll`).addEventListener(`click`, function () {
-  let rollValue = diceRoll();
-  dice.src = `dice-${rollValue}.png`;
+  if (!dice.classList.contains(`hidden`) && player.heldScore <= 100) {
+    checkScore();
+    checkPlayer();
+
+    let rollValue = diceRoll();
+
+    if (rollValue === 1) {
+      score = 0;
+
+      alterRolledScore(score, score);
+      newPlayer();
+    } else {
+      score += rollValue;
+
+      alterRolledScore(score, score);
+    }
+  }
+});
+
+document.querySelector(`.btn--hold`).addEventListener(`click`, function () {
+  checkPlayer();
+  heldScore += score;
+  player.heldScore += heldScore;
+  currentScore.textContent = player.heldScore;
+  rolledScore.textContent = 0;
+
+  checkScore();
+  newPlayer();
+  resetScores();
 });
 
 document.querySelector(`.btn--new`).addEventListener(`click`, function () {
-  resetScore();
+  resetGame();
+  diceRoll();
+  dice.classList.remove(`hidden`);
 });
